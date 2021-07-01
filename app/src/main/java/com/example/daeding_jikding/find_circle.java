f27db0f774b8cfcb79534fc03e103f9f43177cf4
fansor <dlckdduq1107@gmail.com>
2021-07-01 21:57:29 +0900

    create_bottom_navigation

---
+++ app/src/main/java/com/example/daeding_jikding/find_circle.java
@@ -0,0 +1,27 @@
+package com.example.daeding_jikding;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link find_circle#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class find_circle extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_find_circle, container, false);
+    }
+}
